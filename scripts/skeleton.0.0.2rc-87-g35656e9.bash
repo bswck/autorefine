4dff1d00278dcb8a4ccd7a20ac397218515492d1
bswck <bswck.dev@gmail.com>
2024-01-23 15:54:09 +0100

    Upgrade to bswck/skeleton@0.0.2rc-87-g35656e9
    Skeleton revision: https://github.com/bswck/skeleton/tree/0.0.2rc-87-g35656e9

--- scripts/skeleton.0.0.2rc-87-g35656e9.bash
+++ scripts/skeleton.0.0.2rc-87-g35656e9.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-83-g3b14b33).
+# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-87-g35656e9).
 #
-# This file was generated from bswck/skeleton@0.0.2rc-83-g3b14b33.
+# This file was generated from bswck/skeleton@0.0.2rc-87-g35656e9.
 # Instead of changing this particular file, you might want to alter the template:
-# https://github.com/bswck/skeleton/tree/0.0.2rc-83-g3b14b33/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
+# https://github.com/bswck/skeleton/tree/0.0.2rc-87-g35656e9/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
 #
 # Usage:
 #
@@ -33,7 +33,7 @@ trap 'rm -f "$TEMPFILE"' EXIT
 # shellcheck disable=SC1090
 source "$TEMPFILE"
 
-mkcontext() {
+make_context() {
 	export POETRY_VERSION
 	POETRY_VERSION=$(cat <<- 'EOF'
 		1.7.1
@@ -126,32 +126,32 @@ mkcontext() {
 	)
 	export SKELETON_REF
 	SKELETON_REF=$(cat <<- 'EOF'
-		0.0.2rc-83-g3b14b33
+		0.0.2rc-87-g35656e9
 	EOF
 	)
 	export SREF
 	SREF=$(cat <<- 'EOF'
-		0.0.2rc-83-g3b14b33
+		0.0.2rc-87-g35656e9
 	EOF
 	)
 	export SKELETON_REV
 	SKELETON_REV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-83-g3b14b33
+		https://github.com/bswck/skeleton/tree/0.0.2rc-87-g35656e9
 	EOF
 	)
 	export SREV
 	SREV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-83-g3b14b33
+		https://github.com/bswck/skeleton/tree/0.0.2rc-87-g35656e9
 	EOF
 	)
 	export SKELETON_AND_REF
 	SKELETON_AND_REF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-83-g3b14b33
+		bswck/skeleton@0.0.2rc-87-g35656e9
 	EOF
 	)
 	export SNREF
 	SNREF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-83-g3b14b33
+		bswck/skeleton@0.0.2rc-87-g35656e9
 	EOF
 	)
 	export GH_REPO_ARGS
@@ -164,11 +164,11 @@ mkcontext() {
 		jq -n '{"deployment_branch_policy": {"protected_branches": false,"custom_branch_policies": true}}' | gh api -H "Accept: application/vnd.github+json" -X PUT "/repos/bswck/autorefine/environments/$1" --input -
 	EOF
 	)
-    export LAST_REF="0.0.2rc-83-g3b14b33"
+    export LAST_REF="0.0.2rc-87-g35656e9"
     export PROJECT_PATH_KEY="$$_skeleton_project_path"
     export NEW_REF_KEY="$$_skeleton_new_ref"
     export LAST_LICENSE_NAME="GPL-3.0"
 }
 
-mkcontext
+make_context
 update_entrypoint "$@"
