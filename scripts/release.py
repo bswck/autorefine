3e6da3801e2716bf2dc1363b0148088d555ec5a7
bswck <bswck.dev@gmail.com>
2024-01-24 04:06:39 +0100

    Upgrade to bswck/skeleton@0.0.2rc-90-gb227938
    Skeleton revision: https://github.com/bswck/skeleton/tree/0.0.2rc-90-gb227938

--- scripts/release.py
+++ scripts/release.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# This file was generated from bswck/skeleton@0.0.2rc-89-gaa02725.
+# This file was generated from bswck/skeleton@0.0.2rc-90-gb227938.
 # Instead of changing this particular file, you might want to alter the template:
-# https://github.com/bswck/skeleton/tree/0.0.2rc-89-gaa02725/project/scripts/release.py.jinja
+# https://github.com/bswck/skeleton/tree/0.0.2rc-90-gb227938/project/scripts/release.py.jinja
 #
 """
 Automate the release process by updating local files, creating and pushing a new tag.
