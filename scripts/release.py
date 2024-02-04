ed4fee1039cb037a33aa54b6f70bdb5397574c5a
bswck <bswck.dev@gmail.com>
2024-02-04 18:32:14 +0100

    Upgrade to bswck/skeleton@0.0.2rc-134-g0ac2307
    Skeleton revision: https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307

--- scripts/release.py
+++ scripts/release.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# This file was generated from bswck/skeleton@0.0.2rc-132-gf51a888.
+# This file was generated from bswck/skeleton@0.0.2rc-134-g0ac2307.
 # Instead of changing this particular file, you might want to alter the template:
-# https://github.com/bswck/skeleton/tree/0.0.2rc-132-gf51a888/project/scripts/release.py.jinja
+# https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307/project/scripts/release.py.jinja
 #
 """
 Automate the release process by updating local files, creating and pushing a new tag.
--- scripts/skeleton.0.0.2rc-134-g0ac2307.bash
+++ scripts/skeleton.0.0.2rc-134-g0ac2307.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-132-gf51a888).
+# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307).
 #
-# This file was generated from bswck/skeleton@0.0.2rc-132-gf51a888.
+# This file was generated from bswck/skeleton@0.0.2rc-134-g0ac2307.
 # Instead of changing this particular file, you might want to alter the template:
-# https://github.com/bswck/skeleton/tree/0.0.2rc-132-gf51a888/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
+# https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
 #
 # Usage:
 #
@@ -126,32 +126,32 @@ make_context() {
 	)
 	export SKELETON_REF
 	SKELETON_REF=$(cat <<- 'EOF'
-		0.0.2rc-132-gf51a888
+		0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export SREF
 	SREF=$(cat <<- 'EOF'
-		0.0.2rc-132-gf51a888
+		0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export SKELETON_REV
 	SKELETON_REV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-132-gf51a888
+		https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export SREV
 	SREV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-132-gf51a888
+		https://github.com/bswck/skeleton/tree/0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export SKELETON_AND_REF
 	SKELETON_AND_REF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-132-gf51a888
+		bswck/skeleton@0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export SNREF
 	SNREF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-132-gf51a888
+		bswck/skeleton@0.0.2rc-134-g0ac2307
 	EOF
 	)
 	export GH_REPO_ARGS
@@ -164,7 +164,7 @@ make_context() {
 		jq -n '{"deployment_branch_policy": {"protected_branches": false,"custom_branch_policies": true}}' | gh api -H "Accept: application/vnd.github+json" -X PUT "/repos/bswck/autorefine/environments/$1" --input -
 	EOF
 	)
-    export LAST_REF="0.0.2rc-132-gf51a888"
+    export LAST_REF="0.0.2rc-134-g0ac2307"
     export PROJECT_PATH_KEY="$$_skeleton_project_path"
     export NEW_REF_KEY="$$_skeleton_new_ref"
     export LAST_LICENSE_NAME="GPL-3.0"
